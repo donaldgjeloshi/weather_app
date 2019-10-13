73f2feb7c6e8c5901fc86cdafb13c26f90789796
Donald Gjeloshi <dongje11@htl-shkoder.com>
2019-10-13 23:34:59 +0200

    add new functions to the vue, some functions doesn't yet work so good

--- script.js
+++ script.js
@@ -1,27 +1,65 @@
+const OWMBaseUrl = "https://api.openweathermap.org/data/2.5/weather?q=";
+const ApiKey = "c44375da74fcad4a7a252ad4e831ec43";
+
+
+function buildUrl(city) {
+    return OWMBaseUrl + city + "&units=metric&lang=de&appid=" + ApiKey;
+}
+
 new Vue({
     el: "#weatherApp",
     data: {
-        temp: 0,
         format: 'C',
         time: new Date(),
-        phase: 'fa-adjust'
+        phase: 'fa-adjust',
+        apiResults: '',
+        location: '',
+        cityName: '',
+        temp: 0,
+        geoCoordinates: '',
+        cloud: '',
+        humidity: ''
+        
+
     },
     methods: {
-        setTime: function(){
+        setTime: function () {
             var timeAtLoad = this.time;
             return timeAtLoad.toLocaleTimeString('it-IT');
         },
-        setPhase: function() {
+        setPhase: function () {
             var timeOfDayUTC = new Date().getUTCHours();
             var timeOfDay = timeOfDayUTC + 8;
-            
+
             if (timeOfDay > 6 && timeOfDay < 18) {
                 return 'fa-sun'
             } else {
                 return 'fa-moon'
             }
+        },
+        getWeather(city) {
+            let url = buildUrl(city);
+            axios
+                .get(url)
+                .then(response => {
+                    this.apiResults = response.data;
+                })
+                .catch(error => {
+                    console.log(error);
+                });
+        },
+        getData() {
+            this.getWeather(this.location);
+            
+            this.cloud = "Bewölkung: " + this.apiResults.clouds.all + " " + this.apiResults.weather[0].description;
+            this.temp = this.apiResults.main.temp;
+            this.humidity = "Luftfeuchtigkeit: " + this.apiResults.main.humidity + "%";
+            this.geoCoordinates = "Coordinates: [" + this.apiResults.coord.lat + ", " + this.apiResults.coord.lon + "]";
+        },
+        onSubmit() {
+            this.getData();
+
+
         }
     }
-
-
 });
